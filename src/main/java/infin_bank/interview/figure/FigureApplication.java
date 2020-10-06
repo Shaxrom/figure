dbeb4fbc5cc186109b7e1373d8e695164021eaf0
ShukurovSh <59442362+Shaxrom@users.noreply.github.com>
2020-10-06 18:08:13 +0500

    first commit

---
+++ src/main/java/infin_bank/interview/figure/FigureApplication.java
@@ -0,0 +1,13 @@
+package infin_bank.interview.figure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigureApplication.class, args);
+	}
+
+}
